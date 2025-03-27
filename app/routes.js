1bf85846b4032cfcfe8627d07745eb651bff63d8
Ruwaydah-Shariffali <Ruwaydah.shariffali@digital.homeoffice.gov.uk>
2025-03-27 16:03:32 +0000

    updates

--- app/routes.js
+++ app/routes.js
@@ -11,16 +11,16 @@ router.use(radioButtonRedirect)
 
 // Add your routes here
 
-router.post('/public-facing/checkbox-redirect-me', function (req, res) {
-    // Get the answer from session data
-    // The name between the quotes is the same as the 'name' attribute on the input elements
-    // Javascript doesn't support hyphens ('-') in variable names (like formValue)
-  
-    let formValue = req.session.data['place-of-work']
-  
-    if (formValue === 'Hospitals') {
-      res.redirect('public-facing/hospitals')
-    } else { 
-      res.redirect('/public-facing/cya-place-of-work')
-    }
-  })
+router.post('/public-facing/place-of-work/place-of-work', function (req, res) {
+  // Get the answer from session data
+  // The name between the quotes is the same as the 'name' attribute on the input elements
+  // Javascript doesn't support hyphens ('-') in variable names (like formValue)
+
+  let formValue = req.session.data['place-of-work']
+
+  if (formValue.includes('Hospitals')) {
+    res.redirect('/public-facing/place-of-work/hospitals')
+  } else {
+    res.redirect('/public-facing/place-of-work/cya-place-of-work')
+  }
+})
